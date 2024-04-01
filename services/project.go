d28b3a7a1e77eba62fb15916434fd8471d28364e
Fernando Dourado <fernando.dourado@harness.io>
2024-04-01 16:40:20 -0300

    updated project validation

--- services/project.go
+++ services/project.go
@@ -7,34 +7,32 @@ import (
 	"github.com/Fernando-Dourado/harness-move-project/model"
 )
 
-const LIST_PROJECTS = "/ng/api/projects/list"
+const GET_PROJECT = "/ng/api/projects/{identifier}"
 
 func (api *ApiRequest) ValidateProject(org, project string) error {
 
 	resp, err := api.Client.R().
 		SetHeader("x-api-key", api.Token).
 		SetHeader("Content-Type", "application/json").
+		SetPathParam("identifier", project).
 		SetQueryParams(map[string]string{
-			"accountIdentifier":  api.Account,
-			"orgIdentifiers":     org,
-			"projectIdentifiers": project,
-			"pageIndex":          "0",
-			"pageSize":           "1",
+			"accountIdentifier": api.Account,
+			"orgIdentifier":     org,
 		}).
-		Get(BaseURL + LIST_PROJECTS)
+		Get(BaseURL + GET_PROJECT)
 	if err != nil {
 		return err
 	}
 	if resp.IsError() {
 		return handleErrorResponse(resp)
 	}
-	result := model.ProjectExistResult{}
+	result := model.GetProjectResponse{}
 	err = json.Unmarshal(resp.Body(), &result)
 	if err != nil {
 		return err
 	}
 
-	if result.Data.PageItemCount != 1 || result.Data.Empty {
+	if result.Data == nil {
 		return fmt.Errorf("org %s or project %s not exist", org, project)
 	}
 	return nil
