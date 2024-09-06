fa6b9f68222c0d6f74ce4538afe5dddd30412c3f
Joe Fecht <joe.fecht@ahead.com>
2024-09-06 12:11:21 -0500

    Added Targets

---
+++ model/targets.go
@@ -0,0 +1,59 @@
+package model
+
+type TargetListResponse struct {
+	ItemCount int64    `json:"itemCount"`
+	PageCount int64    `json:"pageCount"`
+	PageIndex int64    `json:"pageIndex"`
+	PageSize  int64    `json:"pageSize"`
+	Version   int64    `json:"version"`
+	Targets   []Target `json:"targets"`
+}
+
+type Target struct {
+	Account     string          `json:"account"`
+	Anonymous   bool            `json:"anonymous"`
+	Attributes  Attributes      `json:"attributes"`
+	CreatedAt   int64           `json:"createdAt"`
+	Environment string          `json:"environment"`
+	Identifier  string          `json:"identifier"`
+	Name        string          `json:"name"`
+	Org         string          `json:"org"`
+	Project     string          `json:"project"`
+	Segments    []TargetSegment `json:"segments"`
+}
+
+// Also Referenced by Target Groups
+type Attributes struct {
+	Age      int64  `json:"age"`
+	Location string `json:"location"`
+}
+
+type TargetSegment struct {
+	CreatedAt    int64         `json:"createdAt"`
+	Environment  string        `json:"environment"`
+	Excluded     []Cluded      `json:"excluded"`
+	Identifier   string        `json:"identifier"`
+	Included     []Cluded      `json:"included"`
+	ModifiedAt   int64         `json:"modifiedAt"`
+	Name         string        `json:"name"`
+	Rules        []Rule        `json:"rules"`
+	ServingRules []ServingRule `json:"servingRules"`
+	Tags         []Tag         `json:"tags"`
+	Version      int64         `json:"version"`
+}
+
+// Also Referenced by Target Groups
+type Rule struct {
+	Attribute string        `json:"attribute"`
+	ID        int64         `json:"id"`
+	Negate    bool          `json:"negate"`
+	Op        string        `json:"op"`
+	Values    []interface{} `json:"values"`
+}
+
+// Also Referenced by Target Groups
+type ServingRule struct {
+	Clauses  []interface{} `json:"clauses"`
+	Priority int64         `json:"priority"`
+	RuleID   string        `json:"ruleId"`
+}
---
+++ services/targets.go
@@ -0,0 +1,131 @@
+package services
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/jf781/harness-move-project/model"
+	"github.com/schollz/progressbar/v3"
+)
+
+const TARGETGROUPS = "/cf/admin/segments"
+
+type TargetGroupContext struct {
+	api           *ApiRequest
+	sourceOrg     string
+	sourceProject string
+	targetOrg     string
+	targetProject string
+}
+
+func NewTargetGroups(api *ApiRequest, sourceOrg, sourceProject, targetOrg, targetProject string) TargetGroupContext {
+	return TargetGroupContext{
+		api:           api,
+		sourceOrg:     sourceOrg,
+		sourceProject: sourceProject,
+		targetOrg:     targetOrg,
+		targetProject: targetProject,
+	}
+}
+
+func (c TargetGroupContext) Move() error {
+
+	envs, err := c.api.listEnvironments(c.sourceOrg, c.sourceProject)
+	if err != nil {
+		return err
+	}
+
+	bar := progressbar.Default(int64(len(envs)), "Target Groups    ")
+	var failed []string
+
+	for _, env := range envs {
+		e := env.Environment
+		targets, err := c.api.listTargets(c.sourceOrg, c.sourceProject, e.Identifier)
+		if err != nil {
+			failed = append(failed, fmt.Sprintf("Unable to list targets for environment %s [%s]", env.Environment.Name, err))
+			continue
+		}
+
+		bar.ChangeMax(bar.GetMax() + len(targets))
+
+		for _, target := range targets {
+			i := target
+			// newYaml := createYaml(i.Yaml, c.sourceOrg, c.sourceProject, c.targetOrg, c.targetProject)
+
+			err := c.api.createTarget(&model.Target{
+				Name:        i.Name,
+				Identifier:  i.Identifier,
+				Org:         c.targetOrg,
+				Project:     c.targetProject,
+				Environment:    i.Environment,
+				Attributes: i.Attributes,
+				Segments:   i.Segments,
+			})
+			if err != nil {
+				failed = append(failed, fmt.Sprintln(e.Name, "/", i.Name, "-", err.Error()))
+			}
+			bar.Add(1)
+		}
+		bar.Add(1)
+	}
+	bar.Finish()
+
+	reportFailed(failed, "targets:")
+	return nil
+}
+
+func (api *ApiRequest) listTargets(org, project, envId string) ([]*model.Target, error) {
+
+	resp, err := api.Client.R().
+		SetHeader("x-api-key", api.Token).
+		SetHeader("Content-Type", "application/json").
+		SetQueryParams(map[string]string{
+			"accountIdentifier":     api.Account,
+			"orgIdentifier":         org,
+			"projectIdentifier":     project,
+			"environmentIdentifier": envId,
+			"size":                  "1000",
+		}).
+		Get(BaseURL + TARGETGROUPS)
+	if err != nil {
+		return nil, err
+	}
+	if resp.IsError() {
+		return nil, handleErrorResponse(resp)
+	}
+
+	result := model.TargetListResponse{}
+	err = json.Unmarshal(resp.Body(), &result)
+	if err != nil {
+		return nil, err
+	}
+
+	targets := []*model.Target{}
+	for _, c := range result.Targets {
+		target := c
+		targets = append(targets, &target)
+	}
+
+	return targets, nil
+}
+
+func (api *ApiRequest) createTarget(target *model.Target) error {
+
+	resp, err := api.Client.R().
+		SetHeader("x-api-key", api.Token).
+		SetHeader("Content-Type", "application/json").
+		SetBody(target).
+		SetQueryParams(map[string]string{
+			"accountIdentifier": api.Account,
+			"orgIdentifier": target.Org,
+		}).
+		Post(BaseURL + TARGETGROUPS)
+	if err != nil {
+		return err
+	}
+	if resp.IsError() {
+		return handleErrorResponse(resp)
+	}
+
+	return nil
+}
