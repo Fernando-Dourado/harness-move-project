b938c959679063957b99e40ddef28a45961252b4
Joe Fecht <joe.fecht@ahead.com>
2024-09-10 12:50:54 -0500

    Updated to use new version of CLI, added ability run against list of projects imported by csv

--- main.go
+++ main.go
@@ -6,79 +6,86 @@ import (
 
 	"github.com/fatih/color"
 	"github.com/jf781/harness-move-project/operation"
-	"github.com/urfave/cli"
+	"github.com/urfave/cli/v2"
 )
 
 var Version = "development"
 
 func main() {
-	app := cli.NewApp()
-	app.Name = "harness-move-project"
-	app.Version = Version
-	app.Usage = "Non-official Harness CLI to move project between organizations"
-	app.Action = run
-	app.Flags = []cli.Flag{
-		cli.StringFlag{
-			Name:     "api-token",
-			Usage:    "TODO: Explain the api-token usage",
-			Required: true,
-		},
-		cli.StringFlag{
-			Name:     "account",
-			Usage:    "TODO: Explain the arg usage",
-			Required: true,
-		},
-		cli.StringFlag{
-			Name:     "source-org",
-			Usage:    "TODO: Explain the arg usage",
-			Required: true,
-		},
-		cli.StringFlag{
-			Name:     "source-project",
-			Usage:    "TODO: Explain the arg usage",
-			Required: true,
-		},
-		cli.StringFlag{
-			Name:     "target-org",
-			Usage:    "TODO: Explain the arg usage",
-			Required: true,
-		},
-		cli.StringFlag{
-			Name:     "target-project",
-			Usage:    "TODO: Explain the arg usage",
-			Required: false,
+	app := &cli.App{
+		Name: "harness-copy-project",
+		Version: Version,
+		Usage: "Non-official Harness CLI to copy project between organizations",
+		Action: run,
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:     "csvPath",
+				Usage:    "The path to the CSV file that contains the source and target project information.",
+				Required: true,
+			},
+			&cli.StringFlag{
+				Name:  "apiToken",
+				Usage: "The API token that will be used to authenticate with the Harness Account.",
+				Required: true,
+			},
+			&cli.StringFlag{
+				Name:  "accountId",
+				Usage: "The account ID that contains both the source and target orgnaizations.",
+				Required: true,
+			},
 		},
 	}
 	app.Run(os.Args)
 }
 
-func run(c *cli.Context) {
-	mv := operation.Move{
-		Config: operation.Config{
-			Token:   c.String("api-token"),
-			Account: c.String("account"),
-		},
-		Source: operation.NoName{
-			Org:     c.String("source-org"),
-			Project: c.String("source-project"),
-		},
-		Target: operation.NoName{
-			Org:     c.String("target-org"),
-			Project: c.String("target-project"),
-		},
+
+
+func run(c *cli.Context) error {
+	importCsv := operation.ImportCSV{
+		CsvPath: c.String("csvPath"),
 	}
 
-	applyArgumentRules(&mv)
-
-	if err := mv.Exec(); err != nil {
-		fmt.Println(color.RedString(fmt.Sprint("Failed:", err.Error())))
-		os.Exit(1)
+	csvData, err := importCsv.Exec()
+	if err != nil {
+		fmt.Println(color.RedString(fmt.Sprint("Failed to pull CSV data:", err.Error())))
 	}
-}
 
-func applyArgumentRules(mv *operation.Move) {
-	// USE SOURCE PROJECT AS TARGET, WHEN TARGET NOT SET
-	if len(mv.Target.Project) == 0 {
-		mv.Target.Project = mv.Source.Project
+	for i := 0; i < len(csvData.SourceOrg); i++ {
+		mv := operation.Move{
+			Config: operation.Config{
+				Token:   c.String("apiToken"),
+				Account: c.String("accountId"),
+			},
+			Source: operation.NoName{
+				Org:     csvData.SourceOrg[i],
+				Project: csvData.SourceProject[i],
+			},
+			Target: operation.NoName{
+				Org:     csvData.TargetOrg[i],
+				Project: csvData.TargetProject[i],
+			},
+		}
+
+		 // Check for missing or empty values
+		 if mv.Source.Org == "" || mv.Source.Project == "" || mv.Target.Org == "" {
+			fmt.Println("Invalid CSV data. Missing required fields.")
+			continue // Skip this iteration if required data is missing
+		}
+
+		// Use source project name if target project name is missing
+		if mv.Target.Project == "" {
+			mv.Target.Project = mv.Source.Project
+		}
+
+		fmt.Println(color.GreenString("Moving project '%v' from org '%v' to org '%v'. The target project will be named '%v'", mv.Source.Project, mv.Source.Org, mv.Target.Org, mv.Target.Project)) 
+
+		// Execute the copy operation from source to target operation
+		if err := mv.Exec(); err != nil {
+			fmt.Println(color.RedString(fmt.Sprint("Failed to Copy Project:", err.Error())))
+			os.Exit(1)
+		}
 	}
+
+	return nil
+
 }
