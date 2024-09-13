479b36a5aa54c421d9b0b1cd0f7916c1a4954bdc
Joe Fecht <joe.fecht@gmail.com>
2024-09-13 04:06:56 -0500

    Added logging and ability to count # of projects and rest calls

--- main.go
+++ main.go
@@ -3,15 +3,47 @@ package main
 import (
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/fatih/color"
 	"github.com/jf781/harness-move-project/operation"
+	"github.com/jf781/harness-move-project/services"
 	"github.com/urfave/cli/v2"
+	"go.uber.org/zap"
 )
 
 var Version = "development"
 
+var logger *zap.Logger
+
 func main() {
+
+	// Initlize the logger
+	logger = zap.Must(zap.NewProduction())
+	startTime := time.Now()
+
+	// Defer the logger and print the final log
+	defer logger.Sync()
+	defer func() {
+		stopTime := time.Now()
+		apiCalls := services.GetApiCalls()
+		projects := services.GetProjects()
+
+		duration := stopTime.Sub(startTime)
+
+		logger.Info("Harness Copy Project has completed.",
+			zap.Int("Number of API Calls: ", apiCalls),
+			zap.String("Run Duration: ", duration.String()),
+			zap.Int("Number of projects moved: ", projects),
+			zap.String("Stop Time: ", stopTime.Format("12:00:00")),
+		)
+	}()
+
+	// Start logging
+	logger.Info("Harness Copy Project has started.")
+	logger.Info("Start time: " + startTime.Format("12:00:00"))
+
+	// Create a new CLI app
 	app := &cli.App{
 		Name:    "harness-copy-project",
 		Version: Version,
@@ -40,6 +72,8 @@ func main() {
 			},
 		},
 	}
+
+	// Run the CLI app
 	app.Run(os.Args)
 }
 
@@ -50,15 +84,24 @@ func run(c *cli.Context) error {
 
 	csvData, err := importCsv.Exec()
 	if err != nil {
-		fmt.Println(color.RedString(fmt.Sprint("Failed to pull CSV data:", err.Error())))
+		logger.Error("Failed to pull CSV data",
+			zap.String("csvPath", c.String("csvPath")),
+			zap.Error(err),
+		)
+		return err
 	}
 
 	for i := 0; i < len(csvData.SourceOrg); i++ {
-		mv := operation.Move{
+		// Increment the number of projects moved
+		services.IncrementProjects()
+
+		// Create a new copy operation
+		cp := operation.Copy{
 			Config: operation.Config{
 				Token:   c.String("apiToken"),
 				Account: c.String("accountId"),
 				BaseURL: c.String("baseUrl"),
+				Logger:  logger,
 			},
 			Source: operation.NoName{
 				Org:     csvData.SourceOrg[i],
@@ -71,25 +114,33 @@ func run(c *cli.Context) error {
 		}
 
 		// Check for missing or empty values
-		if mv.Source.Org == "" || mv.Source.Project == "" || mv.Target.Org == "" {
-			fmt.Println("Invalid CSV data. Missing required fields.")
+		if cp.Source.Org == "" || cp.Source.Project == "" || cp.Target.Org == "" {
+			logger.Warn("Invalid CSV data. Missing required fields.",
+				zap.String("Source Org", cp.Source.Org),
+				zap.String("Source Project", cp.Source.Project),
+				zap.String("Target Org", cp.Target.Org),
+			)
 			continue // Skip this iteration if required data is missing
 		}
 
 		// Use source project name if target project name is missing
-		if mv.Target.Project == "" {
-			mv.Target.Project = mv.Source.Project
+		if cp.Target.Project == "" {
+			cp.Target.Project = cp.Source.Project
 		}
 
-		fmt.Println(color.GreenString("Moving project '%v' from org '%v' to org '%v'. The target project will be named '%v'", mv.Source.Project, mv.Source.Org, mv.Target.Org, mv.Target.Project))
+		fmt.Println(color.GreenString("Moving project '%v' from org '%v' to org '%v'. The target project will be named '%v'", cp.Source.Project, cp.Source.Org, cp.Target.Org, cp.Target.Project))
 
 		// Execute the copy operation from source to target operation
-		if err := mv.Exec(); err != nil {
-			fmt.Println(color.RedString(fmt.Sprint("Failed to Copy Project:", err.Error())))
-			os.Exit(1)
+		if err := cp.Exec(); err != nil {
+			logger.Error("Failed to Copy Project",
+				zap.String("Source Project", cp.Source.Project),
+				zap.String("Target Project", cp.Target.Project),
+				zap.Error(err),
+			)
+			return err
 		}
+
+		logger.Info(fmt.Sprintf("Project '%v' has been copied to '%v'", cp.Source.Project, cp.Target.Project))
 	}
-
 	return nil
-
 }
